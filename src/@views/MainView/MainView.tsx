c6edb5d45c50b1db894b3484a1dd5d37ce951d62
Alexandr Siryj <theobroma222@gmail.com>
2022-05-04 03:44:33 +0300

    MainView cleanup

--- src/@views/MainView/MainView.tsx
+++ src/@views/MainView/MainView.tsx
@@ -1,66 +1,31 @@
 // https://stackoverflow.com/questions/67909356/is-there-any-way-to-fetch-all-the-responses-stored-in-api-slice-rtk-query
-import { Container, Grid } from '@mui/material';
+import { Container } from '@mui/material';
 import Box from '@mui/material/Box';
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import ImageGallery from '../../@components/ImageGallery';
-import { useAppDispatch, useAppSelector } from '../../@store/configureStore';
+import { useAppDispatch } from '../../@store/configureStore';
 import { useGetPicturesQuery } from '../../@store/pictures/api';
-import { HitsEntityType } from '../../@types';
 
 const MainView = () => {
   const dispatch = useAppDispatch();
-
-  // const [noMoreResults, setNoMoreResults] = useState(false);
-  const [page, setPage] = useState(2);
-  // const [hits, setHits] = useState<HitsEntityType[]>([]);
-
+  const [page, setPage] = useState(1);
   // const { data, error, isLoading, isFetching, isError }
-
-  const lastResult = useGetPicturesQuery(page - 1, {
-    skip: page === 1,
-  });
-
   const currentResult = useGetPicturesQuery(page);
-  const nextResult = useGetPicturesQuery(page + 1);
 
-  // const pageSize = 12;
-  // const combined = useMemo(() => {
-  //   const arr = new Array(pageSize * (page + 1));
-  //   for (const data of [lastResult.data, currentResult.data, nextResult.data]) {
-  //     if (data) {
-  //       arr.splice(data.offset, data.items.length, ...data.items);
-  //     }
-  //   }
-  //   return arr;
-  // }, [pageSize, page, lastResult.data, currentResult.data, nextResult.data]);
-
-  const lastResultHits = lastResult.data?.hits;
+  // const currentResultHits = currentResult.data?.hits;
   const currentResultHits = currentResult.data?.hits;
-  const nextResultHits = nextResult.data?.hits;
-
-  // console.log('lastResultHits', lastResultHits);
-  // console.log('currentResultHits', currentResultHits);
-  // console.log('nextResultHits', nextResultHits);
-
-  // const combined = [].concat(lastResultHits, currentResultHits, nextResultHits);
-
-  // useEffect(() => {
-  //   console.log(data);
-  // }, [data]);
-
-  // const {
-  //   data: { coins },
-  //   isFetching,
-  // } = useAppSelector(coinsSelector);
-
-  // useEffect(() => {
-  //   dispatch(getCoinsTC());
-  // }, [dispatch]);
+  const total = currentResult.data?.total;
+  console.log('🚀 ~ file: MainView.tsx ~ line 18 ~ MainView ~ total', total);
+  const totalHits = currentResult.data?.totalHits;
+  console.log(
+    '🚀 ~ file: MainView.tsx ~ line 20 ~ MainView ~ totalHits',
+    totalHits,
+  );
 
   return (
     <Container maxWidth="lg">
       <Box sx={{ flexGrow: 1 }}>MainView</Box>
-      <ImageGallery hits={nextResultHits} />
+      <ImageGallery hits={currentResultHits} />
     </Container>
   );
 };
